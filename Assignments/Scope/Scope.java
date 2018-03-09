57cd7f5fe3eca3103dd551bd381dbf6ac3ed08ba
Fernando Rivero <River.Fern2019@ssusd.org>
2018-03-09 07:40:40 -0800

    Adds Scopes

--- Assignments/Scope/Scope.java
+++ Assignments/Scope/Scope.java
@@ -3,36 +3,49 @@
         public static Boolean shouldLoop = false;
 
         public static void main(String args[]) {
-            int i = 1;
-            int n = 1;
+        //     int i = 1;
+        //     int n = 1;
 
-            while(i <= 10) {
-                n = i * 2;
-                System.out.println(n);
-                i++;
-            }
+        //     while(i <= 10) {
+        //         n = i * 2;
+        //         System.out.println(n);
+        //         i++;
+        //     }
 
-            if(n > 10) {
-                System.out.println(message);
-            }
+        //     if(n > 10) {
+        //         System.out.println(message);
+        //     }
 
-            Boolean shouldLoop = false;
-            i = 0;
-            while(shouldLoop) {
-                if(i < 10) {
-                    System.out.println(message);                    
-                } else {
-                    shouldLoop = false;
-                }
-                i++;
-            }
+        //     Boolean shouldLoop = false;
+        //     i = 0;
+        //     while(shouldLoop) {
+        //         if(i < 10) {
+        //             System.out.println(message);                    
+        //         } else {
+        //             shouldLoop = false;
+        //         }
+        //         i++;
+        //     }
 
-            String message = "Hello World";
-            for(i = 0; i < message.length(); i++) {
-            if(message.charAt(i) == 'W') {
-                System.out.println("W found at index " + i);
-                break;
-            }
+        //     String message = "Hello World";
+        //     for(i = 0; i < message.length(); i++) {
+        //     if(message.charAt(i) == 'W') {
+        //         System.out.println("W found at index " + i);
+        //         break;
+        //     }
+        // }
+class Powers {
+    public static final int n = 32;
+    public static int base = 2;
+
+    public static void main(String[] args) {
+        for(int i = 0; i <= n; i++) {
+            power();
+            System.out.println(power);
         }
     }
- }
+
+    public static int power() {
+        
+    }
+}
