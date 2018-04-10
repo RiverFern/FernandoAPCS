763e107b83dde10aafaa6485acfd62bfa19209c3
Fernando Rivero <River.Fern2019@ssusd.org>
2018-04-10 07:40:01 -0700

    First Commit

--- Assignments/twoDarrays/twoDarrays.java
+++ Assignments/twoDarrays/twoDarrays.java
@@ -7,7 +7,6 @@ class twoDarrays {
         //[0,0,0]
 
         int[][] arr1 = new int[3][3];
-
         for(int i = 0; i < arr1.length; i++) {
             for(int j = 0; j < arr1[i].length; j++) {
                 arr1[i][j] = (i * 2) * (j * 2);
@@ -16,26 +15,24 @@ class twoDarrays {
             //[0,4,8]
             //[0,8,16]
         }
-        System.out.println("Value=" + arr1[2][1]);
-
         int[][] arr2 = new int[3][3];
         for(int i = 0; i < arr2.length; i++) {
             for(int j = 0; j < arr2[i].length; j++) {
                 arr2[i][j] = arr1[i][j] * arr1[i][j];
+                System.out.print(arr2[i][j] + ", ");                
             }
             //[0,0,0]
             //[0,16,64]
             //[0,64,256]
         }
-        System.out.println(arr2);
-
         int[][] arr3 = new int[5][5];
-
         int num = 1;
         for(int i = 0; i < arr3.length; i++) {
             for(int j = 0; j < arr3[i].length; j++) {
                 arr3[i][j] = num; 
                 num *= 2;
+                //System.out.print(arr3[i][j] + ", ");
+
             }
             //[1,2,4,8,16]
             //[32,64,128,256,512]
@@ -43,9 +40,23 @@ class twoDarrays {
             //[]
             //[]
         }
-        System.out.println(arr3);
-    
-    
-
+        int[][] arr4 = new int[4][4];
+        int num1 = 1;
+        for(int i = 0; i < arr4.length; i++) {
+            for(int j = 0; j < arr4[i].length; j++) {
+                arr4[i][j] = num1;
+                num1 *= 3;
+                //System.out.print(arr4[i][j] + ", ");
+            }
+        }
+        
+        int[][] arr5 = new int[5][5];
+        System.out.println();
+        for(int i = 0; i < arr5.length; i++) {
+            for(int j = 0; j < arr5[i].length; j++) {
+                arr5[i][j] = (i * (-5)) - (j + 1);
+                System.out.print(arr5[i][j] + ", ");
+            }
+        }
     }
 }
